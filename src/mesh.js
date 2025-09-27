3b0484d73168dfe17e23c6463463cdd4181845f7
gerwyn <gerwynsim@hotmail.com>
2025-09-27 11:04:49 +0800

    geometries

---
+++ src/mesh.js
@@ -0,0 +1,44 @@
+// mesh: most common 3d object: eg. cubes
+// points -> collection of small dots in 3d space: stars/particles
+
+// line -> simple connection between points in 3d space
+// draw shapes, grids or wireframes
+
+// group -> collection of multiple objects
+// transform many objects together
+
+// sprite, a 3d object that always faces the camera (always visible)
+// billboards, icons, particles etc in a scene
+
+// lights and cameras are NOT 3d objects but rather separate entities or tools that influence how 3d objects are seen.
+
+// All elements in three.js, extend "Object3D" properties.
+// Common: position, rotation, scale
+
+// All usually starts with a mesh:
+// spinning cube,
+// realistic game character
+// simple 3D logo on a website
+// complex animated character
+
+// Mesh consists of:
+// block of clay: geometry
+// paint and polish: material
+
+// Geometry: The points/vertices, edges, and surfaces of the shape.
+// Material: Color, texture, and how it reacts to light.
+
+// a cube has 8 vertices and 12 edges
+
+// Material
+// Set solid colors or gradients
+// Apply image textures (like wood or metal)
+// Control how light reflects or passes through
+
+// Explore different types of geometries
+// Customize materials for cooler effects
+// How to animate meshes
+
+// Meshes: Foundation of everything you're build in 3D
+  // Geometry: Shape/Form & Structure
+  // Material: Style/Look
